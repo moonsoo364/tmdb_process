a6727a8a59e21709cd323baef2e3ae6f41809f3c
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-07-30 10:13:49 +0900

    feat) eslint 정리, mongoDB crud 설정

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// import * as api from './src/api/index.js';
 import * as service from './src/service/index.js'
-// api.fetchTv();
+
 service.searchTvShows();
--- src/service/tv_service.js
+++ src/service/tv_service.js
@@ -1,5 +1,15 @@
-import { fetchTv } from '../api/index.js';
+import { fetchTv } from '../external_api/index.js';
+import {
+  clearTvShowCollection,
+  connectToDB,
+  disconnect,
+  insertTvShow
+} from '../database/tv_crud.js';
 
 export const searchTvShows = async () => {
-  const data = await fetchTv();
+  const { results } = await fetchTv();
+  await connectToDB();
+  await insertTvShow(results);
+  await clearTvShowCollection();
+  disconnect();
 };
