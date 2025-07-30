b9c16fba28e5ec1867ce93c33e8f5f9a6db11918
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-07-30 17:59:24 +0900

    feat) 불필요 코드 주석 처리

--- src/middleware/route.js
+++ src/middleware/route.js
@@ -1,8 +1,8 @@
 export function setupRoutes(app) {
-  app.use((req, res, next) => {
-    console.log('모든 요청에 대해 실행됩니다.');
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   console.log('모든 요청에 대해 실행됩니다.');
+  //   next();
+  // });
 
   app.post(/^\/tv-show.*/, (req, res, next) => {
     console.log('tv-show로 시작하는 POST 요청에만 실행됩니다.');
